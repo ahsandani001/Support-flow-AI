25f7a3c89d35b31a4bce3554769b86fdabfdd009
Ahsan Danish <58282492+ahsandani001@users.noreply.github.com>
2026-03-17 14:54:06 +0500

    feat: implement message CRUD with MongoDB
    - Create Message model
    - Implement REST endpoints for messages (addMessage to ticket, GetMessage to Ticket)
    - Test with Requestly - all operations working

---
+++ src/controllers/message.controller.ts
@@ -0,0 +1,51 @@
+import express from 'express';
+import { Message } from '../models/messages.model.ts';
+
+type Request = express.Request;
+type Response = express.Response;
+
+export class MessageController {
+  /**
+   * Add a message to a ticket
+   * POST /tickets/:id/messages
+   */
+  static async addMessage(req: Request, res: Response) {
+    try {
+      const ticketId = Array.isArray(req.params.id)
+        ? req.params.id[0]
+        : req.params.id;
+
+      const messageData = {
+        ticketId,
+        ...req.body,
+      };
+
+      const message = await Message.create(messageData);
+      console.log('message successfully created ==================> ');
+      res.status(201).json(message);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Failed to add message' });
+    }
+  }
+
+  /**
+   * Get all messages for a ticket
+   * GET /tickets/:id/messages
+   */
+  static async getMessages(req: Request, res: Response) {
+    try {
+      const ticketId = Array.isArray(req.params.id)
+        ? req.params.id[0]
+        : req.params.id;
+
+      const messages = await Message.find({ ticketId }).sort({
+        createdAt: 1,
+      });
+      res.json({ Total: messages.length, messages });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Failed to fetch messages' });
+    }
+  }
+}
